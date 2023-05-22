13d16a88a6960e2ca1aa57e7c25b905200106726
Yash <yash.rachh@hiddenbrains.in>
2023-05-22 10:52:55 +0530

    Added option to add title.

--- custom_bottom_navigation/src/main/java/com/example/custom_bottom_navigation/bottom_bar/LottieModel.kt
+++ custom_bottom_navigation/src/main/java/com/example/custom_bottom_navigation/bottom_bar/LottieModel.kt
@@ -1,12 +1,14 @@
 package com.example.custom_bottom_navigation.bottom_bar
 
 import android.widget.RelativeLayout
+import android.widget.TextView
 import com.airbnb.lottie.Lottie
 import com.airbnb.lottie.LottieAnimationView
 
 data class LottieModel(
     var parent : RelativeLayout ?= null,
     var lottie : LottieAnimationView ?= null,
-    var id : String = ""
+    var id : String = "",
+    var titleView : TextView ?= null
 ) {
 }
